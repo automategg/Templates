d597cdcd57becf79547568d82e8f7f4692cf1856
Automate.gg <info@automate.gg>
2020-11-30 17:28:21 -0500

    Updated to support 80 char line length

--- Update-Console.psm1
+++ Update-Console.psm1
@@ -5,12 +5,13 @@
 .DESCRIPTION  
     Console output with settings to change the font color and add a timestamp.
 
-    Output with a font color is great at presenting status or unexpected values. Using Gray for requests, info
-    or benign status, using white for system status or higher status, using green for success or completed, 
-    using yellow and red for try, catch blocks, or using blue for highlightable actions.
+    Output with a font color is great at presenting status or unexpected values. Using Gray for requests, info or benign status, using white for system status or higher status, using green for success or completed, 
+    using yellow and red for try, catch blocks, or using blue for highlightable 
+    actions.
 
-    Output with a timestamp works very well for console monitoring actions being performed by the script. Timestamping
-    works even better when using transcripts or other logging functionality to troubleshoot issues or to audit tasks.
+    Output with a timestamp works very well for console monitoring actions being performed by the script. Timestamping works even better when using 
+    transcripts or other logging functionality to troubleshoot issues or to 
+    audit tasks.
 
 .NOTES  
     File Name       :   Update-Console.psm1
